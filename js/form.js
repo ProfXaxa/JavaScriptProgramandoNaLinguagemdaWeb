8aef81280699f97d816ffa0710f5c02d7f7a8d79
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2025-11-28 10:47:40 +0000

    obtemPacienteDoFormulario

--- js/form.js
+++ js/form.js
@@ -3,13 +3,13 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
     var form = document.querySelector("#form-adiciona");
-
+    
     var paciente = obtemPacienteDoFormulario(form);
     
     var pacienteTr = montarTr(paciente);
-
+    
     var tabela = document.querySelector("#tabela-pacientes");
-
+    
     tabela.appendChild(pacienteTr);
     
     form.reset();
@@ -26,29 +26,22 @@ function obtemPacienteDoFormulario(form){
     }
     return paciente;
 }
-
-
-
+function montarTr(pacinte){
     var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+    
+    pacienteTr.appendChild(montarTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montarTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montarTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(motarTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montarTd(paciente.imc, "info-imc"));
+    return pacienteTr;
+}
 
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
-});
+function montaTd(dado, classe){
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+    
+    return td;
+}
